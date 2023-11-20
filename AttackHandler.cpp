bc91171e172531bc4b87eff7db1678ee1e753cf8
Anderkat178 <108509495+Anderkat178@users.noreply.github.com>
2023-11-20 11:57:15 +0200

    Update AttackHandler.cpp

--- AttackHandler.cpp
+++ AttackHandler.cpp
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2006-2010 - Frictional Games
+ * 
  *
- * This file is part of Penumbra Overture.
+ * 
  *
  * Penumbra Overture is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
